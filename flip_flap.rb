b8f25cce9cba5e68575088cf0e47e8bde7bfa829
eduardo-salazar <esalazar922@gmail.com>
2015-10-03 20:55:13 +0800

    Fix tsv file to mach convert.Removed new line at the end and fix to convertion from tsv to yml

--- flip_flap.rb
+++ flip_flap.rb
@@ -2,28 +2,21 @@ require_relative 'tsv_buddy'
 require 'yaml'
 # This class convert from tsv to yml and viceverse
 class FlipFlap
+  include TsvBuddy
   # Do NOT create an initialize method
   def take_yaml(yml)
     @data = YAML.load(yml)
   end
 
   def to_yaml
-    # Get header names and add a tab between elements
-    result = yml[0].keys.reduce { |a, e| "#{a}\t#{e}" }
-
-    # Get values of objects and add them in a new line separated by tabs
-    @data.each do |element|
-      result += "\n#{element.values.reduce { |a, e| "#{a}\t#{e}" }}"
-    end
-
-    @data = result
+    @data = @data.to_yaml
   end
 end
 
-# class Tester
-#     include TsvBuddy
-#  end
+class Tester
+    include TsvBuddy
+end
 
-# t = Tester.new
-# result = t.take_tsv(File.read('./data/survey.tsv'))
-# puts t.to_tsv
+t = Tester.new
+result = t.take_tsv(File.read('./data/survey.tsv'))
+puts t.to_tsv
