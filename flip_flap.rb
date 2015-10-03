8d749e8c99e2e3845eb0eec190e4747ae4943cee
eduardo-salazar <eduardo@incuapps.com>
2015-10-03 18:04:59 +0800

    Added code to make program run

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,30 @@
 require_relative 'tsv_buddy'
-
+require 'yaml'
+# This class convert from tsv to yml and viceverse
 class FlipFlap
   # Do NOT create an initialize method
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
 
+  def to_yaml
+  	# Get header names and add a tab between elements
+	result = yml[0].keys.reduce { |a, e| "#{a}\t#{e}" }
+
+	# Get values of objects and add them in a new line separated by tabs
+	@data.each do |element|
+	  result += "\n#{element.values.reduce { |a, e| "#{a}\t#{e}" }}"
+	end
+
+	@data = result
+
+  end
 end
+
+# class Tester
+#     include TsvBuddy
+#  end
+
+# t = Tester.new
+# result = t.take_tsv(File.read('./data/survey.tsv'))
+# puts t.to_tsv
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,16 +3,33 @@ module TsvBuddy
   # @data should be a data structure that stores information
   #  from TSV or Yaml files. For example, it could be an Array of Hashes.
   attr_accessor :data
-
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    header = []
+    @data ||= []
+    tsv.each_line.with_index do |line,index|
+      if index == 0
+        # Get header names
+        header = line.gsub("\n", '').split("\t")
+      else
+        # Get tuple and add as an obj
+        @data << Hash[header.zip line.gsub("\n", '').split("\t")]
+      end
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
 
+    result = @data[0].keys.reduce { |a, e| "#{a}\t#{e}" }
+
+    # Get values of objects and add them in a new line separated by tabs
+    @data.each do |element|
+      result += "\n#{element.values.reduce { |a, e| "#{a}\t#{e}" }}"
+    end
+
+    result
   end
 end
